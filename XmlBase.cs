0797f5fb14fe00f2dfed8cfa808ed39080d73599
CelticSlash <37378603+CelticSlash@users.noreply.github.com>
2024-11-07 20:20:26 -0300

    Add files via upload

---
+++ XmlBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Abstracts
+{
+    [Serializable()]
+    public abstract class XmlBase
+    {
+        public string ToXML(XmlSerializerNamespaces namespaces = null, bool indent = false)
+        {
+            return Encoding.UTF8.GetString(ToByteArray(namespaces, indent));
+        }
+
+        public byte[] ToByteArray(XmlSerializerNamespaces namespaces = null, bool indent = false)
+        {
+            var stream = ToStream(namespaces, indent);
+
+            return stream.ToArray();            
+        }
+
+        public MemoryStream ToStream(XmlSerializerNamespaces namespaces = null, bool indent = false)
+        {
+            var xml = new XmlSerializer(this.GetType());
+
+            var ms = new MemoryStream();
+            xml.Serialize(ms, this, namespaces);
+
+            ms.Position = 0;
+
+            return ms;
+        }
+    }
+}
---
+++ ZipUtils.cs
@@ -0,0 +1,30 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Abstracts
+{
+    public static class ZipUtils
+    {
+        public static byte[] NewZip(Dictionary<string, Stream> entries)
+        {
+            var outputStream = new MemoryStream();
+
+            using (var zip = new ZipFile())
+            {
+                foreach(var e in entries)
+                {
+                    zip.AddEntry(e.Key, e.Value);
+                }
+
+                zip.Save(outputStream);
+            }
+
+            outputStream.Position = 0;
+            return outputStream.ToArray();
+        }
+    }
+}
